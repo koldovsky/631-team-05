40292622c8d75146290344ea1138398fb309e7ff
Kukurika Sophiia <sophiia.kuku@gmail.com>
2021-09-30 17:27:30 +0300

    Added js

--- js/roundreviews.js
+++ js/roundreviews.js
@@ -1,42 +1,67 @@
 
-const person = [
-    {
-        id: 9,
-        img: "img/man-with-headphones.png",
-        name: "Dean Williams",
-        description: "I often listen to music, and for me, first of all, it is important that the headphones sit comfortably on my head. In the Basside store, Ive found exactly what i needed at a very nice price. The customer servise also works great, and these guys helped me with the choice.",
-        date: "December 15,2020"
-    },
-    {
-        id: 8,
-        img: "img/girl-with-headphones.png",
-        name: "Jessica Gordons",
-        description: "I often listen to music, and for me, first of all, it is important that the headphones sit comfortably on my head. In the Basside store, Ive found exactly what i needed at a very nice price. The customer servise also works great, and these guys helped me with the choice.",
-        date: "March 28,2021"
-    },
-    {
-        id: 7,
-        img: "img/smiling-man.png",
-        name: "Dean Williams",
-        description: "I often listen to music, and for me, first of all, it is important that the headphones sit comfortably on my head. In the Basside store, Ive found exactly what i needed at a very nice price. The customer servise also works great, and these guys helped me with the choice.",
-        date: "May 29,2021"
-    }
-]
-function renderPerson(person) {
-    const person = document.querySelector('.person');
-    for (const reviews of person) {
-        person.innerHTML +=
-        <div class="person1">
-                <img src="${person.img}" id="person1" alt="man-with-headphones">
-            </div>
-            <div class="name1">
-                <h3>"${person.name}"</h3>
-            </div>
-            <div class="text1">
-                <p>"${person.description}"</p>
-            </div>
-            <div class="date1">
-                <h4>"${person.date}"</h4>
-            </div>
-    }
-}
+(function () {
+    const reviewSlides = [
+        `div class="person1">
+        <img src="img/man-with-headphones.png" id="person1" alt="">
+    </div>
+    <div class="name1">
+        <h3>Dean Williams</h3>
+    </div>
+    <div class="text1">
+        <p>"I often listen to music, and for me, first of all, it is important that the headphones sit
+            comfortably on my head. In the Basside store, Ive found exactly what i needed at a very nice price.
+            The customer servise also works great, and these guys helped me with the choice."</p>
+    </div>
+    <div class="date1">
+        <h4>December 15,2020</h4>
+    </div>`,
+    `div class="person2">
+    <img src="img/girl-with-headphones.png" alt="girl-with-headphones" id="person2">
+</div>
+<div class="name2">
+    <h3>Jessica Gordons</h3>
+</div>
+<div class="text2">
+    <p>"I often listen to music, and for me, first of all, it is important that the headphones sit
+        comfortably on my head. In the Basside store, Ive found exactly what i needed at a very nice price.
+        The customer servise also works great, and these guys helped me with the choice."</p>
+</div>
+<div class="date2">
+    <h4>March 28,2021</h4>
+</div>`,
+`div class="person3">
+<img src="img/smiling-man.png" alt="smiling-man" id="person3">
+</div>
+<div class="name3">
+<h3>Eric Smith</h3>
+</div>
+<div class="text1">
+<p>"I often listen to music, and for me, first of all, it is important that the headphones sit
+    comfortably on my head. In the Basside store, Ive found exactly what i needed at a very nice price.
+    The customer servise also works great, and these guys helped me with the choice."</p>
+</div>
+<div class="date3">
+<h4>May 29,2021</h4>
+</div>`,
+];
+
+let firstSlide = 0;
+
+  function showFirstSlide() {
+    const reviewsContainer = document.querySelector(".reviews__type");
+    reviewsContainer.innerHTML = slides[firstSlide];
+  }
+
+  function secondSlide() {
+    firstSlide++;
+    if (firstSlide >= reviewSlides.length) firstSlide = 0;
+    showFirstSlide();
+  }
+
+    setInterval(nextSlide, 5000);
+    reviewSlides(); 
+
+})
+reviewSlides();
+
+
