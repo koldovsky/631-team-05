ebe457d510602f200ff10dd0c38d8d7847c2bad6
volodymyryaniv <erjaniv@ukr.net>
2021-10-06 16:59:47 +0300

    Started make the cart

--- js/modal-cart.js
+++ js/modal-cart.js
@@ -45,3 +45,79 @@ function unlockBodyScroll() {
   body.style.paddingRight = lockPadding;
   body.classList.remove("lock");
 }
+
+//cart
+
+class Cart {
+  constructor() {
+    this.productService = new ProductsService();
+    this.cartContainer = document.querySelector("#modal-cart");
+    this.cart = JSON.parse(localStorage["cart"] || "{}");
+    //  this.addEventListeners();
+    this.updateBadge();
+  }
+
+  //   addEventListeners() {
+  //     this.cartContainer
+  //       .querySelector(".modal-cart__order")
+  //       .addEventListener("click", (ev) => this.order(ev));
+  //   }
+  saveCart() {
+    localStorage["cart"] = JSON.stringify(this.cart);
+  }
+  async renderCart() {
+    let total = 0;
+    let cartDomSting = "";
+    for (const id in this.cart) {
+      const product = await this.productService.getProductById(id);
+      total += product.price * this.cart[id];
+      cartDomSting += ` <article class="modal-cart-product" data-id="${id}">
+                        <img src="img/featured/gbl-gt.png" alt="">
+                        <p class="modal-cart-product__name">GBL-GT</p>
+                        <form action="#" method="GET">
+                            <input class="modal-cart-product__quantity" type="number" min="1" max="100"
+                                value="quantity">
+                        </form>
+                        <p class="modal-cart-product__price">$120</p>
+                        <button class="modal-cart-product__remove">x</button>
+                    </article>`;
+    }
+    cartDomSting += `<p class="modal-cart-product__total-price">Total price:</p>`;
+    this.cartContainer.querySelector(
+      ".modal-cart__product-container"
+    ).innerHTML = cartDomSting;
+  }
+
+  addProduct(id) {
+    this.cart[id] = (this.cart[id] || 0) + 1;
+    this.saveCart();
+    this.updateBadge();
+  }
+  deleteProducts(id) {
+    if (this.cart[id] > 1) {
+      this.cart[id] -= 1;
+    } else {
+      delete this.cart[id];
+    }
+    this.saveCart();
+    this.updateBadge();
+  }
+  async updateBadge() {
+    const { count, cost } = await this.cartLengthandCost();
+    document.querySelector("#cart-badge").innerText = `${count} $${cost.toFixed(
+      2
+    )}`;
+  }
+
+  async cartLengthandCost() {
+    let count = 0;
+    let cost = 0;
+    for (const key in this.cart) {
+      const product = await this.productService.getProductById(key);
+      const quantity = this.cart[key];
+      count += quantity;
+      cost += quantity * product.price;
+    }
+    return { count, cost };
+  }
+}
