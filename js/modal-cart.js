82799144db7e02959dfd2ecc06d705a734ce2c3a
volodymyryaniv <erjaniv@ukr.net>
2021-10-07 09:19:17 +0300

    Added cart icon

--- js/modal-cart.js
+++ js/modal-cart.js
@@ -1,15 +1,15 @@
-const openLinks = document.querySelectorAll(".open-cart");
-const closeLinks = document.querySelectorAll(".close-cart");
-const body = document.querySelector("body");
-const modalCart = document.querySelector(".modal-cart");
-
-let unlock = true;
+const openLinks = document.querySelectorAll(".open-cart"),
+  closeLinks = document.querySelectorAll(".close-cart"),
+  body = document.querySelector("body"),
+  modalCart = document.querySelector(".modal-cart"),
+  addPadding = document.querySelectorAll(".add-padding");
 
 const timeout = 700;
 function openCart() {
   for (i = 0; i < openLinks.length; i++) {
     openLinks[i].addEventListener("click", function (e) {
       modalCart.classList.add("open");
+      document.querySelector(".cart-link").classList.add("hidden");
       lockBodyScroll();
       e.preventDefault();
     });
@@ -21,6 +21,7 @@ function closeCart() {
   for (i = 0; i < closeLinks.length; i++) {
     closeLinks[i].addEventListener("click", function (e) {
       modalCart.classList.remove("open");
+      document.querySelector(".cart-link").classList.remove("hidden");
       unlockBodyScroll();
       e.preventDefault();
     });
@@ -28,6 +29,7 @@ function closeCart() {
   modalCart.addEventListener("click", function (e) {
     if (!e.target.closest(".modal-cart__content")) {
       modalCart.classList.remove("open");
+      document.querySelector(".cart-link").classList.remove("hidden");
       unlockBodyScroll();
     }
   });
@@ -39,6 +41,11 @@ function lockBodyScroll() {
     window.innerWidth - document.querySelector("body").offsetWidth + "px";
   body.style.paddingRight = lockPadding;
   body.classList.add("lock");
+  // if (addPadding.length > 0) {
+  //   for (let j = 0; j < addPadding.length; j++) {
+  //     addPadding[j].style.paddingRight = lockPadding;
+  //   }
+  // }
 }
 function unlockBodyScroll() {
   const lockPadding = 0;
