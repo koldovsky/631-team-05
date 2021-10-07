29908277fb8a0b890e728dc566bb4a9b396df23a
volodymyryaniv <erjaniv@ukr.net>
2021-10-07 15:07:53 +0300

    Fixed tabs js

--- js/tabs.js
+++ js/tabs.js
@@ -22,31 +22,13 @@ const tabs = (function () {
   }
 })();
 
-// (function () {
-//   async function getProducts(productsUrl) {
-//     const requestProducts = await fetch(productsUrl);
-//     const products = await requestProducts.json();
-//     return products;
-//   }
-class ProductList {
-  constructor(cart) {
-    this.cart = cart;
-    this.productService = new ProductsService();
-    this.productService
-      .getProducts()
-      .then(() => this.showProductsTab())
-      .then(() => this.addEventListeners());
-    this.addEventListeners();
+(function () {
+  async function getProducts(productsUrl) {
+    const requestProducts = await fetch(productsUrl);
+    const products = await requestProducts.json();
+    return products;
   }
-  async getProducts() {
-    if (!this.products) {
-      this.products = await (await fetch("audio-products-list.json")).json();
-    }
-    return this.products;
-  }
-
-  async showProductsTab() {
-    const products = await this.productService.getProducts();
+  function showProductsTab(products) {
     for (let i = 0; i < products.length; i++) {
       if (products[i].category === "featured") {
         let productContainer = document.querySelector("#featured");
@@ -87,25 +69,85 @@ class ProductList {
     }
   }
 
-  // async loadAndRenderProduct() {
-  //   const products = await getProducts("audio-products-list.json");
-  //   showProductsTab(products);
-  // }
-  // loadAndRenderProduct();
-
-  addEventListeners() {
-    document
-      .querySelectorAll(".tabs__button")
-      .forEach((button) =>
-        button.addEventListener("click", (event) =>
-          this.handleProductByClick(event)
-        )
-      );
+  async function loadAndRenderProduct() {
+    const products = await getProducts("audio-products-list.json");
+    showProductsTab(products);
   }
+  loadAndRenderProduct();
+})();
 
-  handleProductByClick(event) {
-    const button = event.target;
-    const id = button.dataset.id;
-    this.cart.addProduct(id);
-  }
-}
+// class ProductList {
+//   constructor(cart) {
+//     this.cart = cart;
+//     this.productService = new ProductsService();
+//     this.productService
+//       .getProducts()
+//       .then(() => this.showProductsTab())
+//       .then(() => this.addEventListeners());
+//     this.addEventListeners();
+//   }
+//   async getProducts() {
+//     if (!this.products) {
+//       this.products = await (await fetch("audio-products-list.json")).json();
+//     }
+//     return this.products;
+//   }
+
+//   async showProductsTab() {
+//     const products = await this.productService.getProducts();
+//     for (let i = 0; i < products.length; i++) {
+//       if (products[i].category === "featured") {
+//         let productContainer = document.querySelector("#featured");
+//         productContainer.innerHTML += `<article class="tabs__container">
+//                                 <a class="tabs__image sm" href="#"><img data-id="${products[i].id}" src="${products[i].img}"
+//                                         alt="${products[i].name}"></a>
+//                                 <a class="tabs__product-name " href="">${products[i].name}</a>
+//                                 <p>$${products[i].price}</p>
+//                                 <form action="#" method="get">
+//                                     <button class="tabs__button" type="submit" data-id="${products[i].id}">Add to cart</button>
+//                                 </form>
+//                             </article> `;
+//       }
+//       if (products[i].category === "sale") {
+//         let productContainer = document.querySelector("#sale");
+//         productContainer.innerHTML += `<article class="tabs__container">
+//                                 <a class="tabs__image sm" data-id="${products[i].id} href="#"><img data-id="${products[i].id}" src="${products[i].img}"
+//                                         alt="${products[i].name}"></a>
+//                                 <a class="tabs__product-name " href="">${products[i].name}</a>
+//                                 <p>$${products[i].price}</p>
+//                                 <form action="#" method="get">
+//                                     <button class="tabs__button" type="submit" data-id="${products[i].id}">Add to cart</button>
+//                                 </form>
+//                             </article> `;
+//       }
+//       if (products[i].category === "new") {
+//         let productContainer = document.querySelector("#new");
+//         productContainer.innerHTML += `<article class="tabs__container">
+//                                 <a class="tabs__image sm" href="#"><img data-id="${products[i].id}" src="${products[i].img}"
+//                                         alt="${products[i].name}"></a>
+//                                 <a class="tabs__product-name " href="">${products[i].name}</a>
+//                                 <p>$${products[i].price}</p>
+//                                 <form action="#" method="get">
+//                                     <button class="tabs__button" type="submit" data-id="${products[i].id}">Add to cart</button>
+//                                 </form>
+//                             </article> `;
+//       }
+//     }
+//   }
+
+//   addEventListeners() {
+//     document
+//       .querySelectorAll(".tabs__button")
+//       .forEach((button) =>
+//         button.addEventListener("click", (event) =>
+//           this.handleProductByClick(event)
+//         )
+//       );
+//   }
+
+//   handleProductByClick(event) {
+//     const button = event.target;
+//     const id = button.dataset.id;
+//     this.cart.addProduct(id);
+//   }
+// }
